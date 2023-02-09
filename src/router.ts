2a6967942df0f7eb0d305038abd73d99af448823
qw-null <qinweirz@qq.com>
2023-02-09 11:39:56 +0800

    feat: finish layout fix: checkBox component router file refactor: logo of YOUTH_UI

--- src/router.ts
+++ src/router.ts
@@ -6,29 +6,41 @@
  * 根据 /packages 目录下的组件所生成的组件类侧边导航栏配置，请勿手动修改
  */
 
- import { createRouter, createWebHashHistory, RouterOptions } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
+ import type { RouterOptions } from 'vue-router';
 
- const routes = [{
-    title: '按钮',
-    name: 'Button',
-    path: '/components/Button',
-    component: () => import('packages/components/Button/docs/README.md'),
-  },{
-    title: '输入框',
-    name: 'Input',
-    path: '/components/Input',
-    component: () => import('packages/components/Input/docs/README.md'),
-  },{
-    title: '图标',
-    name: 'Icon',
-    path: '/components/Icon',
-    component: () => import('packages/components/Icon/docs/README.md'),
-  },{
-    title: '布局',
-    name: 'Layout',
-    path: '/components/Layout',
-    component: () => import('packages/components/Layout/docs/README.md'),
-  }];
+ const routes = [
+   {
+     title: '按钮',
+     name: 'Button',
+     path: '/components/Button',
+     component: () => import('packages/components/Button/docs/README.md'),
+   },
+   {
+     title: '输入框',
+     name: 'Input',
+     path: '/components/Input',
+     component: () => import('packages/components/Input/docs/README.md'),
+   },
+   {
+     title: '图标',
+     name: 'Icon',
+     path: '/components/Icon',
+     component: () => import('packages/components/Icon/docs/README.md'),
+   },
+   {
+     title: '图标',
+     name: 'Icon',
+     path: '/components/CheckBox',
+     component: () => import('packages/components/CheckBox/docs/README.md'),
+   },
+   {
+     title: '布局',
+     name: 'Layout',
+     path: '/components/Layout',
+     component: () => import('packages/components/Layout/docs/README.md'),
+   },
+ ];
  
  const routerConfig = {
    history: createWebHashHistory(),
